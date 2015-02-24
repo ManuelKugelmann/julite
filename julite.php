d3e99f9784dbbe4f767dc4f2b525013b1659a3b7
Manuel Kugelmann <public@bitcraft.org>
2015-02-24 17:42:37 +0100

    [Updated] Version

--- julite.php
+++ julite.php
@@ -3,7 +3,7 @@
 Plugin Name: Julite Product Generator
 Plugin URI: http://www.julispace.com
 Description: Product Generator
-Version: 0.0.1
+Version: 0.0.2
 Author: Manuel Kugelmann
 Author URI: http://www.bitcraft.org
 GitHub Plugin URI: TheDraguun/julite
